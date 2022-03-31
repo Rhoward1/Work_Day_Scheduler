6edee3081eca61e407e61feae7b2fc28bf76b462
Rhoward1 <ryanmhoward@gmail.com>
2022-03-31 03:30:21 -0500

    js edits. event listener

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,20 @@
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
+
+$(document).ready(function() {
+    // event listener
+    $(".saveBtn").on("click", function() {
+      // get nearby values
+      var value = $(this).siblings(".description").val();
+      var time = $(this).parent().attr("id");
+  
+      // save in localStorage
+      localStorage.setItem(time, value);
+    });
+  
+
+
+
+
 
 
 
