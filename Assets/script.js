ee3fbb58cf65b0dbe4992fc4a2bb98f8fddb43bf
Rhoward1 <ryanmhoward@gmail.com>
2022-03-31 20:09:33 -0500

    finished coding JS and cleaned up notations

--- Assets/script.js
+++ Assets/script.js
@@ -7,11 +7,52 @@ $(document).ready(function() {
       var value = $(this).siblings(".description").val();
       var time = $(this).parent().attr("id");
   
-      // save in localStorage
-      localStorage.setItem(time, value);
-    });
-  
+        // save in localStorage
+        localStorage.setItem(time, value);
+    })
+        // load saved from local
+        $("#9 .description").val(localStorage.getItem("9"));
+        $("#10 .description").val(localStorage.getItem("10"));
+        $("#11 .description").val(localStorage.getItem("11"));
+        $("#12 .description").val(localStorage.getItem("12"));
+        $("#13 .description").val(localStorage.getItem("13"));
+        $("#14 .description").val(localStorage.getItem("14"));
+        $("#15 .description").val(localStorage.getItem("15"));
+        $("#16 .description").val(localStorage.getItem("16"));
+        $("#17 .description").val(localStorage.getItem("17"));
 
+
+     // get current number of hours
+    var currentHour = moment().hours();
+    
+    // check updated time
+    function timeCheck() {
+       
+    var interval = setInterval(timeCheck, 10000);  
+  
+    // time color change
+      $(".time-block").each(function() {
+        var blockHour = parseInt($(this).attr("id").split("-")[1]);
+  
+        
+        if (blockHour < currentHour) {
+          $(this).addClass("past");
+        } 
+        else if (blockHour === currentHour) {
+          $(this).removeClass("past");
+          $(this).addClass("present");
+        } 
+        else {
+          $(this).removeClass("past");
+          $(this).removeClass("present");
+          $(this).addClass("future");
+        }
+      });
+    }
+  
+    timeCheck();
+  
+});
 
 
 
